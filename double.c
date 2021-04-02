755bdb33b1638842c6ee6e46ad426859c7d6dbe3
Manaswitha31 <81791282+Manaswitha31@users.noreply.github.com>
2021-04-02 09:45:28 +0530

    Create double.c
    Test commit By Pavan

--- double.c
+++ double.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main() {
     double a, b, c, discriminant, root1, root2, realPart, imagPart;
-    printf("Enter coefficients a, b and c: ");
+    printf("Enter1 coefficients a, b and c: ");
     scanf("%lf %lf %lf", &a, &b, &c);
 
     discriminant = b * b - 4 * a * c;
